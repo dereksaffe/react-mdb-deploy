f75b12399a66b83a33c2def1b5c78cd2f3c3ff88
Derek Saffe <desaff@gmail.com>
2020-07-03 12:33:34 -0300

    first commit

---
+++ build/precache-manifest.4ac3f3e270a1fa73541a3c5bc68f7b4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c97664b4ad30d8e5f5a533212bb655f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cf7ae6e22e25ed31fbe",
+    "url": "/static/js/2.d6ea18c1.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.d6ea18c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afe7ad4b79e08403e9a",
+    "url": "/static/js/main.af7b85df.chunk.js"
+  },
+  {
+    "revision": "6b71224ab26933836c3b",
+    "url": "/static/js/runtime-main.9009ef15.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => <div> Whoops! Nothing can be found here...</div>;
+
+export default NotFound;
